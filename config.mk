b07ab2c38171d4310bb5c96d7f825612307947d9
Mateusz Bieganski <m.bieganski@samsung.com>
2024-03-12 16:30:12 +0100

    add riscv64 support. add obligatory ARCH={riscv|arm} param for 'make'

--- config.mk
+++ config.mk
@@ -7,10 +7,22 @@ VERSION = 0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
+ifndef ARCH
+$(error ARCH is not defined. Please specify ARCH={riscv|armv})
+endif
+
+ifeq ($(ARCH), riscv)
+    CC = riscv64-linux-gnu-gcc
+    DL = /lib64/ld-linux-riscv64-lp64d.so.1
+else ifeq ($(ARCH), arm)
+    CC = ./compiler/bin/arm-linux-gnueabi-gcc
+    DL = /usr/lib/ld-linux.so.3
+else
+    $(error Unsupported ARCH: $(ARCH))
+endif
+
 # flags
 CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
-LDFLAGS  =  -lpthread -s -Wl,--export-dynamic,--dynamic-linker=/usr/lib/ld-linux.so.3 -Wl,--as-needed 
+LDFLAGS  =  -lpthread -s -Wl,--export-dynamic,--dynamic-linker=/lib64/ld-linux-riscv64-lp64d.so.1 -Wl,--as-needed 
 
-# compiler and linker
-CC = ./compiler/bin/arm-linux-gnueabi-gcc
